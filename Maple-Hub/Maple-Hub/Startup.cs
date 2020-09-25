5f3849ab545474477e98eeab4672958d074c5050
Ikbendennis <58038071+Ikbendennis@users.noreply.github.com>
2020-09-25 09:32:18 +0200

    added views

--- Maple-Hub/Maple-Hub/Startup.cs
+++ Maple-Hub/Maple-Hub/Startup.cs
@@ -50,7 +50,7 @@ namespace Maple_Hub
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=MapleHub}/{action=Home}/{id?}");
             });
         }
     }
